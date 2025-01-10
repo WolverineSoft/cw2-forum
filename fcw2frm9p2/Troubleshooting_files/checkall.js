debf977c02a89c5403fd375c3dc2ece72a637c3a
Ravi Bhatt <rnbhatt@umich.edu>
2025-01-10 14:43:28 -0800

    add second pages of some boards

---
+++ fcw2frm9p2/Troubleshooting_files/checkall.js
@@ -0,0 +1,47 @@
+var _____WB$wombat$assign$function_____ = function(name) {return (self._wb_wombat && self._wb_wombat.local_init && self._wb_wombat.local_init(name)) || self[name]; };
+if (!self.__WB_pmw) { self.__WB_pmw = function(obj) { this.__WB_source = obj; return this; } }
+{
+  let window = _____WB$wombat$assign$function_____("window");
+  let self = _____WB$wombat$assign$function_____("self");
+  let document = _____WB$wombat$assign$function_____("document");
+  let location = _____WB$wombat$assign$function_____("location");
+  let top = _____WB$wombat$assign$function_____("top");
+  let parent = _____WB$wombat$assign$function_____("parent");
+  let frames = _____WB$wombat$assign$function_____("frames");
+  let opener = _____WB$wombat$assign$function_____("opener");
+
+//checkall .js
+
+function checkCheckBoxes(flag){
+ var frm = document.forms[0]
+ for(var i=0;i<frm.elements.length;i++){
+  if(frm.elements[i].type == "checkbox"){
+     frm.elements[i].checked = flag;
+  }
+ }
+}
+
+//End of checkall------------//
+
+
+}
+/*
+     FILE ARCHIVED ON 18:23:05 Dec 04, 2004 AND RETRIEVED FROM THE
+     INTERNET ARCHIVE ON 22:30:19 Jan 10, 2025.
+     JAVASCRIPT APPENDED BY WAYBACK MACHINE, COPYRIGHT INTERNET ARCHIVE.
+
+     ALL OTHER CONTENT MAY ALSO BE PROTECTED BY COPYRIGHT (17 U.S.C.
+     SECTION 108(a)(3)).
+*/
+/*
+playback timings (ms):
+  captures_list: 0.469
+  exclusion.robots: 0.017
+  exclusion.robots.policy: 0.007
+  esindex: 0.009
+  cdx.remote: 5.96
+  LoadShardBlock: 355.881 (3)
+  PetaboxLoader3.datanode: 296.854 (4)
+  PetaboxLoader3.resolve: 188.479 (2)
+  load_resource: 171.648
+*/
